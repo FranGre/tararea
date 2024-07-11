666a7bb18020a4f85e675fbd49533555ab1d6ecd
fran gregori <frangregori61@gmail.com>
2024-07-11 20:21:55 +0200

    Implemented meilisearch and can search categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,15 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Laravel\Scout\Searchable;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
     protected $guarded = ['id'];
 
     public function tasks(): HasMany
     {
         return $this->hasMany(Task::class);
+    }
+
+    public function toSearchableArray(): array
+    {
+        $array = [];
+
+        $array['title'] = $this->title;
+
+        return $array;
     }
 }
--- resources/views/livewire/categories.blade.php
+++ resources/views/livewire/categories.blade.php
@@ -1,24 +1,43 @@
 <?php
 
-use function Livewire\Volt\{layout, state};
+use function Livewire\Volt\{layout, state, mount, updated};
 use App\Models\Category;
 
 layout('layouts.base');
 
-state(['categories' => Category::where('user_id', Auth::user()->id)->get()]);
+state(['categories', 'items', 'search' => '']);
+
+mount(function () {
+    $this->categories = Category::where('user_id', Auth::user()->id)->get();
+    $this->items = $this->categories;
+});
+
+updated([
+    'search' => function () {
+        if (empty($this->search)) {
+            $this->items = $this->categories;
+            return;
+        }
+        $this->items = Category::search($this->search)->get();
+    },
+]);
 
 ?>
 
 <div class="text-center">
     <x-h1 class="mb-14">Categories</x-h1>
-    <div class="text-right mb-12">
-        <a href="{{ route('category.create') }}" wire:navigate>New Category</a>
+    <div class="flex mb-12 justify-between">
+        <x-input-text wire:model.live.debounce.300ms='search' placeholder="Search Categories..." class="max-w-xs"/>
+        <div>
+            <a href="{{ route('category.create') }}" wire:navigate>New Category</a>
+        </div>
     </div>
-    @if (count($categories) === 0)
+
+    @if (count($items) === 0)
         <p>No hay categorias :(</p>
     @else
         <div class="grid gap-9 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            @foreach ($categories as $category)
+            @foreach ($items as $category)
                 <a href="{{ route('category.edit', $category->id) }}" wire:navigate>
                     <div
                         class="py-6 px-2 rounded-lg shadow border duration-100 border-black
